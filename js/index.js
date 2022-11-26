87db94bd01ce943f0ac1b8cdd93900b976210c6d
Michael-s-b <michelsilva098@gmail.com>
2022-11-26 11:02:26 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,51 +1,55 @@
-//
-let listOfButton = document.querySelectorAll(".drum"); //create a list of buttons which represent the drums
-
-for (let index = 0; index < listOfButton.length; index++) { //add a mouse event listener "click" for each button on the list
-    listOfButton[index].addEventListener("click", (e) => { playDrum(e.target.textContent); });
+"use strict";
+let listOfButton = document.querySelectorAll(".drum");
+for (let index = 0; index < listOfButton.length; index++) {
+    listOfButton[index].addEventListener("click", (e) => {
+        playDrum(e.target.textContent);
+        buttonAnimation(e.target.textContent);
+    });
 }
-
 document.addEventListener("keydown", (e) => {
     playDrum(e.key);
-    console.log(e.key);
-}); //add a keyboard event "keydown" listener
-
-
-//Functions
-function playDrum(key) { //play a sound depending on the button clicked
+    buttonAnimation(e.key);
+});
+function playDrum(key) {
+    if (key === null) {
+        return;
+    }
     let drumSound;
     switch (key) {
-        case "w": drumSound = new Audio("sounds/tom-1.mp3");
+        case "w":
+            drumSound = new Audio("sounds/tom-1.mp3");
             drumSound.play();
             break;
-        case "a": drumSound = new Audio("sounds/tom-2.mp3");
+        case "a":
+            drumSound = new Audio("sounds/tom-2.mp3");
             drumSound.play();
             break;
-        case "s": drumSound = new Audio("sounds/tom-3.mp3");
+        case "s":
+            drumSound = new Audio("sounds/tom-3.mp3");
             drumSound.play();
             break;
-        case "d": drumSound = new Audio("sounds/tom-4.mp3");
+        case "d":
+            drumSound = new Audio("sounds/tom-4.mp3");
             drumSound.play();
             break;
-        case "j": drumSound = new Audio("sounds/snare.mp3");
+        case "j":
+            drumSound = new Audio("sounds/snare.mp3");
             drumSound.play();
             break;
-        case "k": drumSound = new Audio("sounds/crash.mp3");
+        case "k":
+            drumSound = new Audio("sounds/crash.mp3");
             drumSound.play();
             break;
-        case "l": drumSound = new Audio("sounds/kick-bass.mp3");
+        case "l":
+            drumSound = new Audio("sounds/kick-bass.mp3");
             drumSound.play();
             break;
         default:
             break;
     }
 }
-
-
-
-
-
-
-
-
-
+function buttonAnimation(key) {
+    let activeButton = document.querySelector("." + key);
+    activeButton === null || activeButton === void 0 ? void 0 : activeButton.classList.toggle("pressed");
+    setTimeout(() => activeButton === null || activeButton === void 0 ? void 0 : activeButton.classList.toggle("pressed"));
+}
